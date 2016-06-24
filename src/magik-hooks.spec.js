fc10e0a1c354c934a17fd4c8ec2104fad993b291
magikMaker <bjorn@wikkeling.com>
2016-06-24 23:54:30 +0200

    added testing and linting (#1)

---
+++ src/magik-hooks.spec.js
@@ -0,0 +1,12 @@
+var test = require('tape');
+var magikHooks = require('./magik-hooks');
+
+test('it should have a create method', function(t){
+    t.equal(typeof magikHooks.create, 'function');
+    t.end();
+});
+
+test('it should have a remove method', function(t){
+    t.equal(typeof magikHooks.remove, 'function');
+    t.end();
+});
